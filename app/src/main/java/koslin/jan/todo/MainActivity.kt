f1fc761570d2d28bd26e9212f458c874982162d5
koslinj <koslinj@gmail.com>
2024-04-21 17:35:31 +0200

    add working NewTodoDialog

--- app/src/main/java/koslin/jan/todo/MainActivity.kt
+++ app/src/main/java/koslin/jan/todo/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import koslin.jan.todo.dialog.NewTodoDialog
 import koslin.jan.todo.entity.Todo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -12,14 +15,23 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var newTodoButton: FloatingActionButton
     private lateinit var todoAdapter: TodoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        newTodoButton = findViewById(R.id.newTodoButton)
         recyclerView = findViewById(R.id.mainRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        newTodoButton.setOnClickListener {
+//            MaterialAlertDialogBuilder(this)
+//                .setView(R.layout.new_todo_dialog)
+//                .show()
+            NewTodoDialog().show(supportFragmentManager, "newTodoTag")
+        }
 
         GlobalScope.launch(Dispatchers.IO) {
 //            val todo = Todo(0, "todo 2", "Description 2", System.currentTimeMillis())
---
+++ app/src/main/java/koslin/jan/todo/dialog/NewTodoDialog.kt
@@ -0,0 +1,54 @@
+package koslin.jan.todo.dialog
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textfield.TextInputEditText
+import koslin.jan.todo.App
+import koslin.jan.todo.R
+import koslin.jan.todo.entity.Todo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class NewTodoDialog : DialogFragment(R.layout.new_todo_dialog)
+{
+    private lateinit var saveButton: Button
+    private lateinit var title: TextInputEditText
+    private lateinit var desc: TextInputEditText
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //dialog?.window?.setLayout(400, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        saveButton = view.findViewById(R.id.saveButton)
+        title = view.findViewById(R.id.title)
+        desc = view.findViewById(R.id.desc)
+
+        saveButton.setOnClickListener {
+            saveAction()
+        }
+    }
+
+    private fun saveAction()
+    {
+        val titleStr = title.text.toString()
+        val descStr = desc.text.toString()
+        val dueDate = System.currentTimeMillis() // You may replace this with a DatePicker or similar
+
+        val todo = Todo(title = titleStr, description = descStr, dueDate = dueDate)
+
+        // Insert into Room database
+        GlobalScope.launch(Dispatchers.IO) {
+            App.database.todoDao().insert(todo)
+        }
+
+        title.setText("")
+        desc.setText("")
+        dismiss()
+    }
+
+}
