f360d0e973c1c2a789017969aa95126ca628cb34
koslinj <koslinj@gmail.com>
2024-04-26 20:53:11 +0200

    change DatePickerDialog to not use onCreateDialog but onViewCreated

--- app/src/main/java/koslin/jan/todo/dialog/DatePickerDialog.kt
+++ app/src/main/java/koslin/jan/todo/dialog/DatePickerDialog.kt
@@ -3,9 +3,12 @@ package koslin.jan.todo.dialog
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 import android.widget.DatePicker
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -13,31 +16,64 @@ import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
 import koslin.jan.todo.R
 import koslin.jan.todo.viewmodel.DateTimeViewModel
+import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 
-class DatePickerDialog : DialogFragment() {
+class DatePickerDialog : DialogFragment(R.layout.date_picker_layout) {
 
     private lateinit var datePicker: DatePicker
     private lateinit var timeButton: FloatingActionButton
-    private lateinit var root: View
+    private lateinit var timeTextView: TextView
+    private lateinit var negativeButton: Button
+    private lateinit var positiveButton: Button
     private val dateTimeViewModel: DateTimeViewModel by activityViewModels()
     private var year: Int = 0
     private var month: Int = 0
     private var day: Int = 0
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // Inflate the layout
-        root = LayoutInflater.from(requireContext()).inflate(R.layout.date_picker_layout, null, false)
-        datePicker = root.findViewById(R.id.datePicker)
-        timeButton = root.findViewById(R.id.timeButton)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        datePicker = view.findViewById(R.id.datePicker)
+        timeButton = view.findViewById(R.id.timeButton)
+        timeTextView = view.findViewById(R.id.timeTextView)
+        negativeButton = view.findViewById(R.id.negativeButton)
+        positiveButton = view.findViewById(R.id.positiveButton)
+
+        dateTimeViewModel.selectedDateTime.observe(viewLifecycleOwner) { dateTime ->
+            Log.d("ABC", "powinno ustawic tekst")
+
+            val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+            val timeString = timeFormat.format(dateTime)
+            timeTextView.text = timeString
+        }
+
+        // Use the current date as the default date in the picker.
+        val c = Calendar.getInstance()
+        c.time = Date(dateTimeViewModel.selectedDateTime.value!!)
+        year = c.get(Calendar.YEAR)
+        month = c.get(Calendar.MONTH)
+        day = c.get(Calendar.DAY_OF_MONTH)
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        negativeButton.setOnClickListener {
+            dismiss()
+        }
+
+        positiveButton.setOnClickListener {
+            dateTimeViewModel.updateDate(year, month, day)
+            dismiss()
+        }
 
         timeButton.setOnClickListener {
             val picker =
                 MaterialTimePicker.Builder()
                     .setTimeFormat(TimeFormat.CLOCK_24H)
-                    .setHour(12)
-                    .setMinute(10)
+                    .setHour(hour)
+                    .setMinute(minute)
                     .setInputMode(MaterialTimePicker.INPUT_MODE_CLOCK)
                     .setPositiveButtonText("OK")
                     .setNegativeButtonText(getString(R.string.cancel).uppercase())
@@ -53,28 +89,11 @@ class DatePickerDialog : DialogFragment() {
             picker.show(parentFragmentManager, "timePickerTag")
         }
 
-        // Use the current date as the default date in the picker.
-        val c = Calendar.getInstance()
-        c.time = Date(dateTimeViewModel.selectedDateTime.value!!)
-        year = c.get(Calendar.YEAR)
-        month = c.get(Calendar.MONTH)
-        day = c.get(Calendar.DAY_OF_MONTH)
-
         // Initialize the DatePicker with the current date
         datePicker.init(year, month, day) { view, year, month, dayOfMonth ->
             onDateSet(year, month, dayOfMonth)
         }
 
-        // Build AlertDialog with custom view
-        return AlertDialog.Builder(requireContext())
-            .setView(root)
-            .setPositiveButton("OK") { dialog, which ->
-                dateTimeViewModel.updateDate(year, month, day)
-            }
-            .setNegativeButton(R.string.cancel) { dialog, which ->
-
-            }
-            .create()
     }
 
     private fun onDateSet(year: Int, month: Int, dayOfMonth: Int) {
