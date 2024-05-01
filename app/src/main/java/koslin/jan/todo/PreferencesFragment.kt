14727c9d47829a3d4351ed37d0af93a8d9bdee17
koslinj <koslinj@gmail.com>
2024-05-01 17:01:46 +0200

    change switch to be built in SwitchPreferenceCompat

---
+++ app/src/main/java/koslin/jan/todo/PreferencesFragment.kt
@@ -0,0 +1,24 @@
+package koslin.jan.todo
+
+import android.os.Bundle
+import androidx.fragment.app.activityViewModels
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
+import koslin.jan.todo.viewmodel.TodoViewModel
+
+class PreferencesFragment : PreferenceFragmentCompat() {
+    private val todoViewModel: TodoViewModel by activityViewModels()
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        findPreference<SwitchPreferenceCompat>("notifications")
+            ?.setOnPreferenceChangeListener { _, newValue ->
+                newValue as Boolean
+                todoViewModel.showActiveOnly(newValue)
+                true
+            }
+
+    }
+
+}
--- app/src/main/java/koslin/jan/todo/dialog/ModalBottomSheet.kt
+++ app/src/main/java/koslin/jan/todo/dialog/ModalBottomSheet.kt
@@ -5,17 +5,17 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.materialswitch.MaterialSwitch
+import koslin.jan.todo.PreferencesFragment
 import koslin.jan.todo.R
 import koslin.jan.todo.viewmodel.TodoViewModel
 
 class ModalBottomSheet : BottomSheetDialogFragment(R.layout.sheet) {
-    private val todoViewModel: TodoViewModel by activityViewModels()
-    private lateinit var activeTodosSwitch: MaterialSwitch
     private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,25 +26,17 @@ class ModalBottomSheet : BottomSheetDialogFragment(R.layout.sheet) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        activeTodosSwitch = view.findViewById(R.id.activeTodosSwitch)
-
-        val showActiveTodos = sharedPreferences.getBoolean("show_active_todos", false)
-        activeTodosSwitch.isChecked = showActiveTodos
-
-        activeTodosSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
-            sharedPreferences.edit().putBoolean("show_active_todos", isChecked).apply()
-
-            showToast(if (isChecked) "ACTIVE ONLY" else "ALL")
-            todoViewModel.showActiveOnly(isChecked)
-        }
-
         val behavior = (dialog as BottomSheetDialog).behavior
         behavior.state = BottomSheetBehavior.STATE_EXPANDED
         behavior.skipCollapsed = true
-    }
 
-    private fun showToast(message: String) {
-        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+        val preferencesFragmentContainer = view.findViewById<FragmentContainerView>(R.id.preferencesFragmentContainer)
+
+        // Create and add the PreferencesFragment
+        val preferencesFragment = PreferencesFragment()
+        childFragmentManager.beginTransaction()
+            .replace(preferencesFragmentContainer.id, preferencesFragment)
+            .commit()
     }
 
 }
