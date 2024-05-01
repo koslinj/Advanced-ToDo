15f659fe4a03cc0503539fb0091b8f7035f97d80
koslinj <koslinj@gmail.com>
2024-05-01 13:44:01 +0200

    initial switch functionality

--- app/src/main/java/koslin/jan/todo/dialog/ModalBottomSheet.kt
+++ app/src/main/java/koslin/jan/todo/dialog/ModalBottomSheet.kt
@@ -2,20 +2,43 @@ package koslin.jan.todo.dialog
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.materialswitch.MaterialSwitch
 import koslin.jan.todo.R
+import koslin.jan.todo.viewmodel.TodoViewModel
 
 class ModalBottomSheet : BottomSheetDialogFragment(R.layout.sheet) {
+    private val todoViewModel: TodoViewModel by activityViewModels()
+    private lateinit var activeTodosSwitch: MaterialSwitch
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        activeTodosSwitch = view.findViewById(R.id.activeTodosSwitch)
+
+        activeTodosSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                // Show all todos
+                showToast("Showing active todos")
+                todoViewModel.showActiveOnly(true)
+            } else {
+                // Hide completed todos
+                showToast("Showing All todos")
+                todoViewModel.showActiveOnly(false)
+            }
+        }
 
         val behavior = (dialog as BottomSheetDialog).behavior
         behavior.state = BottomSheetBehavior.STATE_EXPANDED
         behavior.skipCollapsed = true
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
 }
