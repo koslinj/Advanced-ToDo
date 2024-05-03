1ed3d89e7c7b20dbe8a7562656ee91498cec30b3
koslinj <koslinj@gmail.com>
2024-05-03 21:20:51 +0200

    working functionality of inserting to db a todo with attachments

--- app/src/main/java/koslin/jan/todo/dialog/NewTodoDialog.kt
+++ app/src/main/java/koslin/jan/todo/dialog/NewTodoDialog.kt
@@ -1,16 +1,21 @@
 package koslin.jan.todo.dialog
 
+import android.app.Activity
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Spinner
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.textfield.TextInputEditText
 import koslin.jan.todo.R
+import koslin.jan.todo.entity.Attachment
 import koslin.jan.todo.entity.Todo
 import koslin.jan.todo.viewmodel.DateTimeViewModel
 import koslin.jan.todo.viewmodel.TodoViewModel
@@ -21,12 +26,21 @@ class NewTodoDialog : DialogFragment(R.layout.new_todo_dialog)
 {
     private lateinit var saveButton: Button
     private lateinit var dateButton: Button
+    private lateinit var attachmentButton: Button
     private lateinit var title: TextInputEditText
     private lateinit var desc: TextInputEditText
     private lateinit var category: Spinner
 
+    private val attachmentUris = mutableListOf<Uri>()
+
     private val dateTimeViewModel: DateTimeViewModel by activityViewModels()
     private val todoViewModel: TodoViewModel by activityViewModels()
+
+    val getContent = registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uris: List<Uri> ->
+        attachmentUris.clear()
+        attachmentUris.addAll(uris)
+        Log.d("FILES", attachmentUris.toString())
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -39,15 +53,15 @@ class NewTodoDialog : DialogFragment(R.layout.new_todo_dialog)
         title = view.findViewById(R.id.title)
         desc = view.findViewById(R.id.desc)
         category = view.findViewById(R.id.categorySpinner)
+        attachmentButton = view.findViewById(R.id.attachmentButton)
+        attachmentButton.setOnClickListener {
+            getContent.launch("*/*")
+        }
 
         saveButton.setOnClickListener {
             saveAction()
         }
 
-//        dateTimeViewModel.selectedDateTime.observe(viewLifecycleOwner) { date ->
-//            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-//            dateButton.text = dateFormat.format(date)
-//        }
 
         // Inside your fragment
         dateTimeViewModel.selectedDateTime.observe(viewLifecycleOwner) { dateTime ->
@@ -78,11 +92,24 @@ class NewTodoDialog : DialogFragment(R.layout.new_todo_dialog)
         val cat = catValues[pos]
 
         val todo = Todo(title = titleStr, description = descStr, dueDate = dueDate, category = cat)
-        todoViewModel.addTodo(todo)
+        //todoViewModel.addTodo(todo)
+        val attachments = createAttachmentsFromUris()
+        todoViewModel.addTodoWithAttachments(todo, attachments)
 
         title.setText("")
         desc.setText("")
         dismiss()
+    }
+
+    private fun createAttachmentsFromUris(): List<Attachment> {
+        val attachments = mutableListOf<Attachment>()
+        for (uri in attachmentUris) {
+            // Assuming you have a method to get the URI string
+            val uriString = uri.toString()
+            val attachment = Attachment(todoId = 0, uri = uriString) // Assuming todoId needs to be set later
+            attachments.add(attachment)
+        }
+        return attachments
     }
 
 }
