3885dd51fe5b47f4c6f8dc41a2853cde9cf33eef
koslinj <koslinj@gmail.com>
2024-05-24 17:14:13 +0200

    add KEY for saving paths in instanceState

--- app/src/main/java/koslin/jan/todo/config/Keys.kt
+++ app/src/main/java/koslin/jan/todo/config/Keys.kt
@@ -4,4 +4,5 @@ object Keys {
     const val VISIBILITY_KEY = "visibility"
     const val BEFORE_KEY = "before"
     const val CATEGORIES_KEY = "categories"
+    const val FILE_PATHS_KEY = "filePaths"
 }
